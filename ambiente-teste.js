cef196e98f78dadcb3ba807103611c2100fad803
Luis Felipe <lipe.lftn@gmail.com>
2021-05-23 14:46:39 -0300

    teste no ambiente para melhorar segurança

--- ambiente-teste.js
+++ ambiente-teste.js
@@ -1,7 +1,16 @@
 var nome = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%-&*"
 var comprimento = nome.length
 
-let aleatorio = Math.round(Math.random()*comprimento)
+let aleatorio;
+let mudar1;
+let mudar2;
+let trocar;
 
-console.log(nome[aleatorio])
-
+for(var i=0;i<5;i++){
+    aleatorio = Math.round(Math.random()*comprimento)
+    mudar1 = Math.round(Math.random()*4)
+    mudar2 = Math.round(Math.random()*4)
+    resultado = nome[aleatorio]
+    trocar =  resultado.replace(resultado[mudar1,mudar2])
+    console.log(resultado)
+}
